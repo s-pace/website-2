b5d91ecff2a40c026a8cd583475814ecedad0a96
s-pace <sylvain.pace@algolia.com>
2020-08-19 19:50:08 +0200

    style(search): use similar style as popperjs

--- src/components/Docsearch.js
+++ src/components/Docsearch.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useRef, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/core';
 import { Link, navigate } from 'gatsby';
 import { DocSearchButton, useDocSearchKeyboardEvents } from '@docsearch/react';
 import '@docsearch/css';
@@ -9,13 +10,32 @@ let DocSearchModal = null;
 
 const Hit = ({ hit, children }) => <Link to={hit.url}>{children}</Link>;
 
+const docsearchCredentials = {
+  appId: 'BH4D9OD16A',
+  apiKey: 'd5fa05c4e33e776fbf2b8021cbc15b37',
+  indexName: 'popper',
+  searchParameters: {
+    facetFilters: [
+      `tags:${window.location.pathname.includes('v1') ? 'v1' : 'v2'}`,
+    ],
+  },
+};
+
+const colors = {
+  brand: '#ff6b81',
+  active: '#d2cbe4',
+  hitActiveBg: '#c83b50',
+  dark: '#2f263c',
+  darker: '#1c1428',
+  divider: '#44395d',
+  black: '#1c1428',
+  permalink: '#ffb6b3',
+  header: '#b886fd',
+  navButton: '#4edee5',
+  starFill: '#ffe69d',
+};
+
 const DocSearch = () => {
-  const docsearchCredentials = {
-    appId: 'BH4D9OD16A',
-    apiKey: 'd5fa05c4e33e776fbf2b8021cbc15b37',
-    indexName: 'popper',
-    algoliaOptions: { facetFilters: ['tags:v2'] },
-  };
   const searchButtonRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
   const [initialQuery, setInitialQuery] = useState(null);
@@ -59,7 +79,7 @@ const DocSearch = () => {
     onInput,
     searchButtonRef,
   });
-  
+
   return (
     <Fragment>
       <Helmet>
@@ -72,11 +92,60 @@ const DocSearch = () => {
           crossOrigin
         />
       </Helmet>
+      <Global
+        styles={css`
+          .DocSearch {
+            a {
+              border-bottom-style: none;
+            }
 
+            .DocSearch-Hit-icon {
+              color: ${colors.permalink};
+            }
+            .DocSearch-Hit[aria-selected='true'] {
+              a {
+                background: ${colors.hitActiveBg};
+              }
+              .DocSearch-Hit-action-button:hover path,
+              .DocSearch-Hit-action-button:focus path {
+                fill: ${colors.starFill};
+              }
+            }
+            .DocSearch-Hit-source,
+            .DocSearch-Prefill {
+              color: ${colors.header};
+            }
+
+            --docsearch-primary-color: ${colors.brand};
+            --docsearch-highlight-color: ${colors.brand};
+
+            --docsearch-modal-background: ${colors.dark};
+
+            --docsearch-searchbox-shadow: inset 0 0 0 2px
+              var(--docsearch-primary-color);
+
+            --docsearch-hit-color: ${colors.active};
+            --docsearch-hit-active-color: ${colors.active};
+            --docsearch-hit-background: ${colors.darker};
+
+            --docsearch-footer-background: ${colors.dark};
+          }
+
+          .DocSearch-Commands {
+            --docsearch-muted-color: ${colors.navButton};
+            --docsearch-key-gradient: linear-gradient(
+              -26.5deg,
+              ${colors.darker} 0%,
+              ${colors.black} 100%
+            );
+          }
+
+          .DocSearch-Button {
+            margin: 0 10px;
+          }
+        `}
+      />
       <DocSearchButton
-        style={{
-          margin: '0 10px',
-        }}
         onTouchStart={importDocSearchModalIfNeeded}
         onFocus={importDocSearchModalIfNeeded}
         onMouseOver={importDocSearchModalIfNeeded}
